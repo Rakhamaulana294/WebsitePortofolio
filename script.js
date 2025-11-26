8a25e822de2b6fae32a19147b9289774674f2b61
Rakhamaulana294 <rakaraka48246@gmail.com>
2025-11-26 13:27:52 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("btn").addEventListener("click",  ()=>{
+    setTimeout( () =>{
+        document.getElementById("nama").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("msg").value = "";
+
+ }, 5)
+ })
